dd05ceb74bde225581f176c3498e252556bed957
Aurora Duncan <aurorabrynn@gmail.com>
2021-04-06 16:13:05 -0700

    all apis are working

--- assets/js/fetchScript.js
+++ assets/js/fetchScript.js
@@ -1,12 +1,18 @@
 
-var dogurlToFetch = `https://boiling-meadow-47923.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all`
-//var caturlToFetch =
-var petToFetch = "https://api.petfinder.com/v2/animals?type=dog&page=2"
+var dogurlToFetch = `https://boiling-meadow-47923.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1`
+var caturlToFetch = `https://meowfacts.herokuapp.com/`
+var petToFetch = `https://api.petfinder.com/v2/animals?type=dog&page=2`
 
 fetch(dogurlToFetch).then(function (response) {
     return response.json()
 }).then(function (data) {
-    //console.log(data);
+    console.log(data);
+});
+
+fetch(caturlToFetch).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    console.log(data);
 });
 
 fetch("https://api.petfinder.com/v2/oauth2/token", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ grant_type: "client_credentials", client_id: "HEGMAK8rmrTdGMxSLLuVTpwt1pAwGMYtsMlYO8XCERMTLT7CAY", client_secret: "CfhtSPVPSW0YKpFQvIHmdDYtBzEWHkFaVmr2nlAe" }) }).then(function (response) {
